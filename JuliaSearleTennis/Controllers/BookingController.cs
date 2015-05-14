4fa440fd69af3c713b7c6bdbed3c5e840720ebb6
Adam <adam.tyson@hotmail.co.uk>
2015-05-14 22:07:49 +0100

    autumn booking form

---
+++ JuliaSearleTennis/Controllers/BookingController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JuliaSearleTennis.Controllers
+{
+    public class BookingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TenLessons()
+        {
+            return View();
+        }
+
+        public ActionResult PrivateLessons()
+        {
+            return View();
+        }
+        // GET: Booking
+        public ActionResult Tots()
+        {
+            return View();
+        }
+
+        public ActionResult Red()
+        {
+            return View();
+        }
+
+        public ActionResult Orange()
+        {
+            return View();
+        }
+
+        public ActionResult Green()
+        {
+            return View();
+        }
+
+        public ActionResult Adults()
+        {
+            return View();
+        }
+
+        public ActionResult Juniors()
+        {
+            return View();
+        }
+
+        public ActionResult TeamTraining()
+        {
+            return View();
+        }
+
+        public ActionResult Cardio()
+        {
+            return View();
+        }
+    }
+}
